0985d743aed4b37b65bef6588170009fc44911ae
Developer <developer@ankit-tyagis-macbook-pro.local>
2011-06-20 10:57:47 -0500

    Initial Commit

---
+++ Zombies/ZombiesAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ZombiesAppDelegate.h
+//  Zombies
+//
+//  Created by Developer on 6/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ZombiesViewController;
+
+@interface ZombiesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ZombiesViewController *viewController;
+
+@end
---
+++ Zombies/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Zombies
+//
+//  Created by Developer on 6/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZombiesAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([ZombiesAppDelegate class]));
+    }
+    return retVal;
+}
